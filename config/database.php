24300bc9d48bade4a3e7ba38287ba1da46aceffa
acecavaliers <95069460+acecavaliers@users.noreply.github.com>
2024-05-19 12:40:44 +0800

    db prefix

--- config/database.php
+++ config/database.php
@@ -87,7 +87,7 @@ return [
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
-            'prefix' => 'hris_',
+            'prefix' => 'lwc_',
             'prefix_indexes' => true,
             // 'encrypt' => env('DB_ENCRYPT', 'yes'),
             // 'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'false'),
