bb405f4403bdacb4b404a99c935e6d53f0febb2e
acecavaliers <95069460+acecavaliers@users.noreply.github.com>
2024-05-25 07:42:08 +0800

    forms2

--- database/migrations/2024_05_20_021316_create_patient_physical_exams_table.php
+++ database/migrations/2024_05_20_021316_create_patient_physical_exams_table.php
@@ -24,7 +24,6 @@ return new class extends Migration
             $table->string('deleted_by')->nullable();
             $table->timestamps();
             $table->softDeletes();
-            $table->timestamps();
         });
     }
 
