8160df1af716376d1fa7f48ac98fa87d1ffd9bff
acecavaliers <95069460+acecavaliers@users.noreply.github.com>
2024-05-24 14:40:53 +0800

    illness emitter

--- resources/js/Pages/Illnesses/Illness.vue
+++ resources/js/Pages/Illnesses/Illness.vue
@@ -2,38 +2,35 @@
 <template>
     <Head title="Dashboard" />
     <MasterLayout>
-        <div>
-            SSSSSSSSSSSSSSSSS
-        </div>
+        <span v-if="layOut == 'main'">
+            <IllnessMain></IllnessMain>
+        </span>
+        <span v-else-if="layOut == 'child'">
+            <IllnessChild></IllnessChild>
+        </span>
     </MasterLayout>
-
-
 </template>
+
 
 <script setup>
 import MasterLayout from '@/Layouts/MasterLayout.vue';
 import { Head } from '@inertiajs/vue3';
-import Form from '@/Pages/Patient/Form.vue'
+import IllnessMain from '@/Pages/Illnesses/IllnessMain.vue'
+import AddIllness from '@/Pages/Illnesses/component/AddIllness.vue'
+import IllnessChild from '@/Pages/Illnesses/component/IllnessChild.vue'
 
-const user = {
-  name: 'Tom Cook',
-  email: 'tom@example.com',
-  imageUrl:
-    'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-}
-const navigation = [
-  { name: 'Dashboard', href: '#', current: true },
-  { name: 'Team', href: '#', current: false },
-  { name: 'Projects', href: '#', current: false },
-  { name: 'Calendar', href: '#', current: false },
-  { name: 'Reports', href: '#', current: false },
-]
-const userNavigation = [
-  { name: 'Your Profile', href: '#' },
-  { name: 'Settings', href: '#' },
-  { name: 'Sign out', href: '#' },
-]
 </script>
 <script>
+export default {
+    data(){
+        return{
+            layOut: 'main'
+        }
+    },
+    created(){
+        this.emitter.on('layOut', (layout) => {
+              this.layOut = layout
+          })
+    }
+}
 </script>
-
--- resources/js/Pages/Illnesses/IllnessMain.vue
+++ resources/js/Pages/Illnesses/IllnessMain.vue
@@ -1,29 +1,35 @@
 <template>
-    <Head title="Dashboard" />
+    <!-- <Head title="Dashboard" /> -->
     <MasterLayout>
-        <div class="mb-3">
-            <!-- Breadcrumb -->
+        <div class="mb-2">
             <nav class="flex px-5 py-3 text-gray-700 border border-gray-200 rounded-lg bg-gray-50" aria-label="Breadcrumb">
-            <ol class="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
-                <li class="inline-flex items-center">
-                <a href="#" class="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="size-5 mr-2">
-                    <path d="M10 2a.75.75 0 0 1 .75.75v1.5a.75.75 0 0 1-1.5 0v-1.5A.75.75 0 0 1 10 2ZM10 15a.75.75 0 0 1 .75.75v1.5a.75.75 0 0 1-1.5 0v-1.5A.75.75 0 0 1 10 15ZM10 7a3 3 0 1 0 0 6 3 3 0 0 0 0-6ZM15.657 5.404a.75.75 0 1 0-1.06-1.06l-1.061 1.06a.75.75 0 0 0 1.06 1.06l1.06-1.06ZM6.464 14.596a.75.75 0 1 0-1.06-1.06l-1.06 1.06a.75.75 0 0 0 1.06 1.06l1.06-1.06ZM18 10a.75.75 0 0 1-.75.75h-1.5a.75.75 0 0 1 0-1.5h1.5A.75.75 0 0 1 18 10ZM5 10a.75.75 0 0 1-.75.75h-1.5a.75.75 0 0 1 0-1.5h1.5A.75.75 0 0 1 5 10ZM14.596 15.657a.75.75 0 0 0 1.06-1.06l-1.06-1.061a.75.75 0 1 0-1.06 1.06l1.06 1.06ZM5.404 6.464a.75.75 0 0 0 1.06-1.06l-1.06-1.06a.75.75 0 1 0-1.061 1.06l1.06 1.06Z" />
-                    </svg>
-                    Home
-                </a>
-                </li>
-                <li aria-current="page">
-                <div class="flex items-center">
-                    <svg class="rtl:rotate-180  w-3 h-3 mx-1 text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
-                    </svg>
-                    <span class="ms-1 text-sm font-medium text-gray-500 md:ms-2">Flowbite</span>
-                </div>
-                </li>
-            </ol>
+                <ol class="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
+                    <li class="inline-flex items-center">
+                    <Link href="illness" class="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600 ">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="size-5 mr-2">
+                        <path d="M10 9a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM6 8a2 2 0 1 1-4 0 2 2 0 0 1 4 0ZM1.49 15.326a.78.78 0 0 1-.358-.442 3 3 0 0 1 4.308-3.516 6.484 6.484 0 0 0-1.905 3.959c-.023.222-.014.442.025.654a4.97 4.97 0 0 1-2.07-.655ZM16.44 15.98a4.97 4.97 0 0 0 2.07-.654.78.78 0 0 0 .357-.442 3 3 0 0 0-4.308-3.517 6.484 6.484 0 0 1 1.907 3.96 2.32 2.32 0 0 1-.026.654ZM18 8a2 2 0 1 1-4 0 2 2 0 0 1 4 0ZM5.304 16.19a.844.844 0 0 1-.277-.71 5 5 0 0 1 9.947 0 .843.843 0 0 1-.277.71A6.975 6.975 0 0 1 10 18a6.974 6.974 0 0 1-4.696-1.81Z" />
+                        </svg>
+                        Illness List
+                    </Link>
+                    </li>
+                    <li>
+                    <div class="flex items-center">
+                        <svg class="rtl:rotate-180 block w-3 h-3 mx-1 text-gray-400 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
+                        </svg>
+                        <Link href="#" class="ms-1 text-sm font-medium text-gray-700 hover:text-blue-600 md:ms-2">New List</Link>
+                    </div>
+                    </li>
+                    <li aria-current="page">
+                    <div class="flex items-center">
+                        <svg class="rtl:rotate-180  w-3 h-3 mx-1 text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
+                        </svg>
+                        <span class="ms-1 text-sm font-medium text-gray-500 md:ms-2">Illness</span>
+                    </div>
+                    </li>
+                </ol>
             </nav>
-
         </div>
         <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
             <div class="flex items-center justify-between flex-column md:flex-row flex-wrap space-y-4 md:space-y-0 py-4 bg-white">
@@ -48,33 +54,57 @@
                 </div>
             </div>
             <div class="">
-                <IllnessList :key="listKey"/>
+                <!-- <IllnessList :key="listKey"/> -->
+                <span v-if="layOut == 'IllnessList'">
+                    <IllnessList :key="listKey"></IllnessList>
+                </span>
+                <span v-else-if="layOut == 'child'">
+                    <IllnessChild :key="listKey"></IllnessChild>
+                </span>
+
             </div>
         </div>
+
 </MasterLayout>
 
 </template>
 
 
 <script setup>
-import { ref } from 'vue';
+// import { ref } from 'vue';
 import MasterLayout from '@/Layouts/MasterLayout.vue';
 import Modal from '@/Layouts/Modal.vue';
-import { Head } from '@inertiajs/vue3';
-import AddIllness from '@/Pages/Illnesses/AddIllness.vue';
-import IllnessList from '@/Pages/Illnesses/IllnessList.vue';
+import { Head, Link } from '@inertiajs/vue3';
+import AddIllness from '@/Pages/Illnesses/component/AddIllness.vue';
+import IllnessList from '@/Pages/Illnesses/component/IllnessList.vue';
+import IllnessChild from '@/Pages/Illnesses/component/IllnessChild.vue';
 
-const showModal = ref(false);
+</script>
 
-const listKey = ref(0);
+<script>
+export default {
+    data(){
+        return{
+            showModal: false,
+            listKey:0,
+            layOut: 'IllnessList',
+        }
+    },
+    methods: {
+        handleFormSubmitted() {
+        this.listKey++;
+        this.showModal = false;
+    },
+    handleModalClose() {
+        this.showModal = false;
+    }
+    },
+    created(){
 
-function handleFormSubmitted() {
-  listKey.value++;
-  showModal.value = false;
-}
-
-function handleModalClose() {
-  showModal.value = false;
+        this.emitter.on('layOut', (layout) => {
+              this.layOut = layout
+          })
+    }
 }
 </script>
 
