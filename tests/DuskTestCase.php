e8008cdf670679699fd268357689125e16cb04ed
acecavaliers <95069460+acecavaliers@users.noreply.github.com>
2024-08-22 14:18:20 +0800

    Dusk install

--- tests/DuskTestCase.php
+++ tests/DuskTestCase.php
@@ -42,7 +42,8 @@ abstract class DuskTestCase extends BaseTestCase
         return RemoteWebDriver::create(
             $_ENV['DUSK_DRIVER_URL'] ?? env('DUSK_DRIVER_URL') ?? 'http://localhost:9515',
             DesiredCapabilities::chrome()->setCapability(
-                ChromeOptions::CAPABILITY, $options
+                ChromeOptions::CAPABILITY,
+                $options
             )
         );
     }
